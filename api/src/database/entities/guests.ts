ee80c032ca269285a89ca30d3721205e483ceae0
Your Name <mthsepfinaio@gmail.com>
2026-06-18 16:05:18 -0300

    criar_guests

---
+++ api/src/database/entities/guests.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('guests')
+export class Guest {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ name: "first_name", type: "varchar" })
+    firtsname: string;
+
+    @Column({ name: "last_name", type: "varchar" })
+    lastsname: string;
+
+    @Column({ name: "email", type: "varchar" })
+    email: string;
+
+    @Column({ name: "cpf", type: "varchar" })
+    cpf: string;
+
+    @Column({ name: "phone", type: "varchar" })
+    phone: string;
+
+    @Column({ name: "event_table", type: "varchar" })
+    eventTable: string;
+
+    @Column({ name: "checkin", type: "boolean"})
+    checkin: boolean;
+}
