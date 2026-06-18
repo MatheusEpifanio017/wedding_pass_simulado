44b882182dde413fd7d867dd6de550ed6392c537
Your Name <mthsepfinaio@gmail.com>
2026-06-18 17:11:35 -0300

    atualizar_index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -12,11 +12,11 @@ async function main(){
         res.json({ name: pkg.name, version: pkg.version })
     });
 
-    app.post("/users/new", );
+    // app.post("/users/new", );
 
-    app.put("/users/update/:id", )
+    // app.put("/users/update/:id", )
 
-    app.delete("/users/delete/:id",)
+    // app.delete("/users/delete/:id",)
 
     app.listen(3000, () => {
         console.log("Servidor rodando na porta 3000")
