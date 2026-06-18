0d6fe7c468fb9339c9341cd80030938a84c61228
Your Name <mthsepfinaio@gmail.com>
2026-06-18 17:03:38 -0300

    atuslizar_index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import pkg from '../package.json';
 import { AppDataSource } from './database/dataSource';
+import { User } from './database/entities/users';
 
 async function main(){
 
@@ -11,6 +12,11 @@ async function main(){
         res.json({ name: pkg.name, version: pkg.version })
     });
 
+    app.post("/users/new", );
+
+    app.put("/users/update/:id", )
+
+    app.delete("/users/delete/:id",)
 
     app.listen(3000, () => {
         console.log("Servidor rodando na porta 3000")
