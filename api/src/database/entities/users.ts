4ad35622b36400a60a7bcb9f36e80eff68497878
Your Name <mthsepfinaio@gmail.com>
2026-06-18 16:05:07 -0300

    criar_users

---
+++ api/src/database/entities/users.ts
@@ -0,0 +1,26 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { UserRole } from "./enum/userRole";
+
+@Entity('users') 
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ name: "email", type: "varchar" })
+    email: string;
+
+    @Column({ name: "password", type: "varchar" })
+    password: string;
+
+    @Column({ name: "fist_name", type: "varchar" })
+    firstname: string;
+
+    @Column({ name: "last_name", type: "varchar" })
+    lastname: string;
+
+    @Column({ name: "cpf", type: "varchar" })
+    cpf: string;
+
+    @Column({ name: "role", type: "enum", enum: UserRole})
+    role: UserRole;
+};
